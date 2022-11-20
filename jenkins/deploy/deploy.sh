769c78266f7e4a7c914c9f512cde1d7beb440274
Jose Ignacio Martin <jose-ignacio.martin@klarna.com>
2022-11-20 17:08:52 +0100

    Change path for scripts

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,8 +4,14 @@ echo jenkins-course-maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-echo "*** current path $PWD ****"
+WORKSPACE=/var/jenkins_home/workspace/pipeline-docker-maven/
+WORKSPACE2=/Users/jose-ignacio.martin/learning/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+
+echo "*** current path $WORKSPACE ****"
+echo "*** current path $WORKSPACE2 ****"
+ls -la $WORKSPACE
+ls -la $WORKSPACE2
 docker cp /tmp/.auth centos-remote-host:/tmp/.auth
-docker cp ./jenkins/deploy/publish centos-remote-host:/tmp/publish
-docker cp ./jenkins/deploy/docker-compose.yml centos-remote-host:/tmp/docker-compose.yml
+docker cp $WORKSPACE/jenkins/deploy/publish centos-remote-host:/tmp/publish
+docker cp $WORKSPACE2/jenkins/deploy/docker-compose.yml centos-remote-host:/tmp/docker-compose.yml
 docker exec -it centos-remote-host bash /tmp/publish
